fc31b1b06faf68aaa48c792485f02268d8c1cfe1
Sebastian Bolaños <xsebaspc5@gmail.com>
2021-06-09 20:19:14 -0500

    09/06/2021 final2

--- myBusTracker/myBusTracker/settings.py
+++ myBusTracker/myBusTracker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'applications.registro',
     'applications.bus',
+
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
